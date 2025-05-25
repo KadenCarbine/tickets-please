9d9ac344aebac9f4b5694ce4d5779112de6eb889
Kaden Carbine <kadenjcar@gmail.com>
2025-05-25 14:50:16 -0400

    Episode 9

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 
 Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
+Route::apiResource('users', UsersController::class)->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
